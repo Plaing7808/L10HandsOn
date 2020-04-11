96598d48789068df98be60c86acecd2ee95c7a8c
Plaing7808 <61950009+Plaing7808@users.noreply.github.com>
2020-04-11 12:25:52 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.github.com/users/plaing7808/repos',
+    true)
+request.onload = function () {
+    var data = JSON.parse(this.response);
+    console.log(data);
+    var statusHTML = '';
+    $.each(data, function (i, status) {
+        statusHTML += '<div class="card"> \
+                <a href=""> \
+                    <h4>' + status.name + '</h4> \
+                    <div class="state"> \
+                        <span class="mr-4"><i class="fa fa-star mr-2"></i>' + status.stargazers_count + '</span> \
+                        <span class="mr-4"><i class="fa fa-code-fork mr-2"></i>' + status.forks_count + '</span> \
+                    </div> \
+                </a> \
+            </div>';
+    });
+    $('.repositories').html(statusHTML);
+}
+request.send();
+
+$(document).ready(function(){
+    $("a").hover(function(){
+        $(this).toggleClass("icon-bar a:hover")
+    });
+});
